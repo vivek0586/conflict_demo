4883802b9fac20f168fae77e1952a33d1d7d4b98
Vivek Kumar <vivek0586@gmail.com>
2022-06-01 22:00:40 -0400

    merge

--- helloworld.py
+++ helloworld.py
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
-def hello_world(state):
-    print(f'Hello World {state}!')
+def hello_world(city, state):
+    print(f'Hello World {city}, {state}!')
 
 
 def main():
-    hello_world('New York')
-=======
-def hello_world(city):
-    print(f'Hello World {city}!')
-
-
-def main():
-    hello_world('NYC')
->>>>>>> 932bfe73f2bf33ae3bdc03eb788d7c17962da5db
+    hello_world('NYC', 'New York')
 
 
 main()
